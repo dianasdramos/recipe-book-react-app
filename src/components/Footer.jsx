3cce0aaadfbdbaa2ce1488cbe7de6ca0aa385f84
Gabriela Silva <gvsde8@gmail.com>
2024-02-07 20:05:26 +0000

    day1 without css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,11 @@ function Footer() {
 
     return (
         <footer>
-            <div>
-                <img src="" />
-            </div>
-            <div>
-                <a href=""><b>Gabriela Silva</b></a>
-                <a href=""><b>Diana Ramos</b></a>
+            <div className="container">
+            <img src="/src/assets/images/linkedin_3536569.png" />
+
+                <a href="https://www.linkedin.com/in/gabrielavieira-da--silva/"><b>Gabriela Silva</b></a>
+                <a href="https://www.linkedin.com/in/dianaramos/"><b>Diana Ramos</b></a>
             </div>
             <div>
                 <p>2024 Ironhack</p>
