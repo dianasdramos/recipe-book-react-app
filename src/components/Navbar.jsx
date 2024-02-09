f8d1de3e0861f38c57a89912a8da613531fb7055
Gabriela Silva <gvsde8@gmail.com>
2024-02-09 15:09:34 +0000

    navbar and images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,14 @@ function Navbar() {
     return (
         <nav>
             <div>
-                <a href=""><img src="" alt="" /></a>
+                <a href=""><img src="../assets/images/menu.png" alt="" /></a>
             </div>
             <div>
-                <h3><b></b></h3>
+                <h3><b>Page Title</b></h3>
             </div>
             <div>
-                <a href=""><img src="" alt="" /></a>
-                <a href=""><img src="" alt="" /></a>
-                <a href=""><img src="" alt="" /></a>
+                <a href=""><img src="../assets/images/search.png" alt="" /></a>
+                <a href=""><img src="../assets/images/more.png" alt="" /></a>
             </div>
         </nav>
     )
